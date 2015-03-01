416a6a4b905dbeaadcb341baf79e63b76b5b1cc9
Brisk <briskgreen@163.com>
2015-03-01 13:07:52 +0800

    更新线程池头文件

--- ppool.h
+++ ppool.h
@@ -6,6 +6,11 @@
 
 enum PPOOL_STATUS{start,stop};
 
+typedef char pbool;
+
+#define PTRUE 1
+#define PFALSE 0
+
 typedef struct
 {
 	PPOOL_STATUS status; //当前线程池的状态
@@ -19,5 +24,14 @@ typedef struct
 	pthread_mutex_t ppool_mux;
 	pthread_cond_t ppool_con;
 }pool_t;
+
+//初始化一个线程池
+pool_t *ppool_init(int pool_max_num);
+
+//向线程池中添加一个任务
+pbool ppool_taskadd(pool_t *pool,pool_w *task);
+
+//销毁一个线程池
+void ppool_destroy(pool_t *pool);
 
 #endif
