441cc0629e08720d4d3afa41050e885da527754f
Brisk <briskgreen@163.com>
2015-03-05 11:17:28 +0800

    更新错误处理

--- ppool_errno.h
+++ ppool_errno.h
@@ -4,10 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int ppool_errno=0; //错误代码变量
+extern int ppool_errno; //错误代码变量
 
 #define PE_OK 0
 #define PE_POOL_NO_MEM -1
+#define PE_THREAD_NO_MEM -2
+#define PE_THREAD_MUTEX_ERROR -3
+#define PE_THREAD_COND_ERROR -4
+#define PE_QUEUE_NO_MEM -5
+#define PE_PRIORITY_ERROR -6
+#define PE_QUEUE_NODE_NO_MEM -7
 
 //打印错误信息
 void perror(const char *msg);
